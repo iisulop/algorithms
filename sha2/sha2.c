fea2c4bb58bdb52cbe9fac225aa96193d8cf5e0b
Iiro Sulopuisto <iisulop@gmail.com>
2015-12-30 22:39:56 +0200

    Moved the API comments from source to header.

--- sha2/sha2.c
+++ sha2/sha2.c
@@ -266,17 +266,6 @@ out:
   return ret;
 }
 
-/*
- * Calculate the SHA256 hash over the given input.
- *
- * @param buf The buffer of data to calculate the hash over.
- * @param len The length of the buf parameter in bytes.
- * @param hash The result of the hash.
- * @param hash_len The length of the result hash.
- *
- * @return Zero on success, non-zero on failure.
- *
- */
 uint8_t
 calculate_sha256 (uint8_t* buf, uint64_t len,
                   uint8_t** hash, uint32_t* hash_len)
--- sha2/sha2.h
+++ sha2/sha2.h
@@ -1,5 +1,16 @@
 #include <stdint.h>
 
+/*
+ * Calculate the SHA256 hash over the given input.
+ *
+ * @param buf The buffer of data to calculate the hash over.
+ * @param len The length of the buf parameter in bytes.
+ * @param hash The result of the hash.
+ * @param hash_len The length of the result hash.
+ *
+ * @return Zero on success, non-zero on failure.
+ *
+ */
 uint8_t
 calculate_sha256 (uint8_t* buf, uint64_t len,
                   uint8_t** hash, uint64_t* hash_len);
